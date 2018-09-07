a369f7079f15cb0b2bcdff911a1550d72f3456ce
Kevin Fang <kevin.fang@headspace.com>
2018-09-07 13:49:29 -0700

    add scala version

---
+++ src/main/scala/SignIn.scala
@@ -0,0 +1,65 @@
+
+trait Session {
+
+}
+
+trait User {
+
+  var email: String
+  var lastLoginAt: Long
+  var session: Session
+  var numLoginAttempts: Int
+
+  /** return true if the passed in password is valid for this User */
+  def checkPassword(p: String): Boolean
+
+  /** returns the updated user with a new session */
+  def generateSession(): User
+
+  /** updates the lastLoginAt date and returns the updated User */
+  def updateLastLogin(): User
+
+  /** sets the number of login attempts, returns the updated User */
+  def setNumLoginAttempts(attempts: Int): User
+
+}
+
+trait UserService {
+
+  /** returns the User matching the email, otherwise returns null */
+  def getUserByEmail(email: String): User
+
+}
+
+class RouteImpls {
+
+  val userService: UserService
+
+  /**
+    * POST "/auth/signin"
+    *
+    * Logs a user in
+    * Requirements:
+    * - password must be correct
+    * - can try up to 3 times
+    * - updates lastloginAt date
+    * - returns a new session
+    */
+  def signin(email: String, password: String): Session = {
+    var user = userService.getUserByEmail(email)
+    user = user.setNumLoginAttempts(user.numLoginAttempts + 1)
+
+    if (user.numLoginAttempts >= 3) {
+      throw new Unauthorized(401, "no more login attempts")
+    }
+
+    if (user.checkPassword(password)) {
+      user.updateLastLogin()
+      user.generateSession()
+      return user.session
+    }
+
+    throw new BadRequest(400, "invalid email or password")
+  }
+
+}
